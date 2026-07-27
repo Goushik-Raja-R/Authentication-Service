41f6f5d70031b30003b2182b7c6817b05e21ec1d
Goushik-Raja-R <goushikraja14@gmail.com>
2026-07-27 21:15:11 +0530

    Implemented Routes

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import {Router} from 'express'
+import authrouter  from "./auth.routes.js";
+import healthrouter from "./health.routes.js";
+
+const router = Router();
+
+router.use("/api/auth",authrouter);
+router.use("/api/health/",healthrouter);
+
+export default router;
